5d6bbcb9cddab215431fe7fd28189eb668ca0e02
Ashish <ashish9gps@gmail.com>
2023-07-18 01:37:36 +0530

    Add files via upload
    Array-Method final commit

---
+++ arraymethod.js
@@ -0,0 +1,31 @@
+// Array Method in JavaScript
+
+
+//concat && join method
+ let a1 = [1,2,3,4,5];
+ let a2 = [3,4,5];
+ let a3 = a1.concat(a2)
+ console.log(a3)
+
+ let s = a3.join("")
+ console.log(s)
+
+let a4 = [2,3,4,5,6];
+ let a5 = [3,4,5,7];
+
+let a6 = a4.concat(a5);
+
+ let r = a6.reverse();
+ console.log(r);
+
+//index of method in javascript
+
+console.log(a1.indexOf(5));
+
+
+//slice method in javascript
+
+let arr3 = [1,2,3,4,5,6];
+console.log(arr3.slice(2,6));
+
+
